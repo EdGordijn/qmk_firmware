4ad300845e659614e3137b058dc349a86eed817a
Gordijn <gordijn_e@schiphol.nl>
2025-04-16 22:11:24 +0200

    changes in keymap

--- keyboards/controllerworks/mini42/keymaps/correspondent/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/correspondent/keymap.c
@@ -23,7 +23,6 @@ enum layers {
   NAV,
   FUN,
   ALTNUM,
-  ALTSYM
 };
 
 
@@ -32,7 +31,9 @@ enum custom_keycodes {
   U_DQUO,
   U_GRV,
   U_QUOT,
-  U_TILD
+  U_TILD,
+  
+  U_EURO               // Extra symbols
 };
 
 // Short aliases for home row mods and other tap-hold keys.
@@ -41,8 +42,8 @@ enum custom_keycodes {
 #define HRM_T LCTL_T(KC_T)
 #define HRM_N LSFT_T(KC_N)
 #define HRM_B RALT_T(KC_B)
-#define HRM_J LT(ALTNUM, KC_J)
-#define HRM_H LT(ALTSYM, KC_H)
+#define HRM_J LT(SYM, KC_J)
+#define HRM_H LT(ALTNUM, KC_H)
 
 #define HRM_A RSFT_T(KC_A)
 #define HRM_E RCTL_T(KC_E)
@@ -50,11 +51,26 @@ enum custom_keycodes {
 #define HRM_QUO RGUI_T(KC_QUOT)
 #define HRM_DOT RALT_T(KC_DOT)
 #define HRM_C LT(ALTNUM, KC_C)
-#define HRM_COMM LT(ALTSYM, KC_COMM)
+#define HRM_COMM LT(SYM, KC_COMM)
 
+#define NAV_TAB LT(NAV, KC_TAB)
+#define FUN_ENT LT(FUN,KC_ENT)
+
+// Num layer
+#define HRM_4 LGUI_T(KC_4)
+#define HRM_3 LALT_T(KC_3)
+#define HRM_2 LCTL_T(KC_2)
+#define HRM_1 LSFT_T(KC_1)
+
+#define HRM_6 RSFT_T(KC_A)
+#define HRM_7 RCTL_T(KC_E)
+#define HRM_8 LALT_T(KC_O)
+#define HRM_9 RGUI_T(KC_QUOT)
+
+
+#define U_EURO ALGR(KC_5)   // International symbol
 
 #define XXX KC_NO           // key available but not used
-
 
 
 // Correspondent 1: An optimized layout for typing Dutch texts.
@@ -63,98 +79,107 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Base
-  [0] = LAYOUT_split_3x6_3(
-      RM_TOGG,         KC_X,            KC_K,            KC_G,            KC_L,            KC_M,
-	  KC_P,            KC_U,            KC_V,            KC_W,            KC_Y,            XXX,
-	  
-      KC_EXLM,         HRM_S,           HRM_D,           HRM_T,           HRM_N,           KC_R,
-	  KC_I,            HRM_A,           HRM_E,           HRM_O,           HRM_QUO,         KC_QUES,
-	  
-      UG_NEXT,         KC_Q,            HRM_B,           HRM_J,           HRM_H,           KC_F,
-	  KC_Z,            HRM_C,           HRM_COMM,        HRM_DOT,         KC_SLSH,         XXX,
-	  
-      KC_ESC,          LT(NAV,KC_SPC),  KC_TAB,          LT(SYM,KC_ENT),  LT(NUM,KC_BSPC), LT(FUN,KC_BSLS)
+  [BASE] = LAYOUT_split_3x6_3(						  
+      RM_TOGG,  KC_X,     KC_K,     KC_G,     KC_L,     KC_M,               KC_P,     KC_U,     KC_V,     KC_W,     KC_Y,     KC_ESC,	  
+      KC_EXLM,  HRM_S,    HRM_D,    HRM_T,    HRM_N,    KC_R,               KC_I,     HRM_A,    HRM_E,    HRM_O,    HRM_QUO,  KC_QUES,
+      UG_NEXT,  KC_Q,     HRM_B,    HRM_J,    HRM_H,    KC_F,               KC_Z,     HRM_C,    HRM_COMM, HRM_DOT,  KC_SLSH,  XXX,
+	                                XXX,      KC_SPC,   NAV_TAB,            FUN_ENT,  KC_BSPC,  XXX
   ),
-  // Numpad
-  [1] = LAYOUT_split_3x6_3(
-      XXX,             KC_LBRC,         KC_7,            KC_8,            KC_9,            KC_RBRC,
-	  XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-	  
-      XXX,             KC_SCLN,         KC_4,            KC_5,            KC_6,            KC_EQL,
-	  XXX,             KC_LSFT,         KC_LCTL,         KC_LALT,         KC_LGUI,         XXX,
-	  
-      XXX,             KC_GRV,          KC_1,            KC_2,            KC_3,            KC_BSLS,
-	  XXX,             XXX,             KC_COMM,         RALT_T(KC_DOT),  XXX,             XXX,
-	  
-      KC_DOT,          KC_0,            KC_MINS,         XXX,             XXX,             XXX
+  // Numpad - Miryoku
+  [NUM] = LAYOUT_split_3x6_3(
+      RM_TOGG,  KC_LBRC,  KC_7,     KC_8,     KC_9,     KC_RBRC,            XXX,      XXX,      XXX,      XXX,      XXX,      XXX,
+      XXX,      KC_SCLN,  KC_4,     KC_5,     KC_6,     KC_EQL,             XXX,      KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  XXX,
+      UG_NEXT,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_BSLS,            XXX,      XXX,      KC_COMM,  RALT_T(KC_DOT),  XXX,             XXX,
+                                    KC_DOT,   KC_0,     KC_MINS,            XXX,      XXX,      XXX
   ),
-  // Sym
-  [2] = LAYOUT_split_3x6_3(
-      KC_GRV,          KC_LCBR,         KC_AMPR,         KC_ASTR,         KC_LPRN,         KC_RCBR,
-	  XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-	  
-      XXX,             KC_COLN,         KC_DLR,          KC_PERC,         U_CIRC,          KC_PLUS,
-	  XXX,             KC_LSFT,         KC_LCTL,         KC_LALT,         KC_LGUI,         XXX,
-	  
-      XXX,             U_TILD,          KC_EXLM,         KC_AT,           KC_HASH,         KC_PIPE,
-	  XXX,             XXX,             KC_COMM,         RALT_T(KC_DOT),  XXX,             XXX,
-	  
-      KC_LPRN,         KC_RPRN,         KC_UNDS,         XXX,             XXX,             XXX
+  // Sym - Miryoku
+  // [SYM] = LAYOUT_split_3x6_3(
+      // KC_GRV,   KC_LCBR,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RCBR,            XXX,      XXX,      XXX,      XXX,      XXX,      XXX,
+      // XXX,      KC_COLN,  KC_DLR,   KC_PERC,  U_CIRC,   KC_PLUS,            XXX,      KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  XXX,
+      // UG_NEXT,  U_TILD,   KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,            XXX,      XXX,      KC_COMM,  RALT_T(KC_DOT),  XXX,             XXX,
+                                    // KC_LPRN,  KC_RPRN,  KC_UNDS,            XXX,      XXX,      XXX
+  // ),
+    // Sym alternative
+  [SYM] = LAYOUT_split_3x6_3(
+      _______,  XXX,      KC_LCBR,  KC_PLUS,  KC_RCBR,  XXX,                XXX,      KC_LT,    KC_EQL,   KC_GT,    XXX,      _______,
+      KC_EXLM,  KC_SCLN,  KC_LPRN,  KC_MINS,  KC_RPRN,  KC_HASH,            KC_PERC,  KC_LBRC,  KC_UNDS,  KC_RBRC,  KC_COLN,  _______,
+      _______,  KC_PIPE,  KC_DLR,   KC_ASTR,  U_EURO,   KC_AT,              KC_AMPR,  U_GRV,    U_CIRC,   U_TILD,   KC_BSLS,  _______,
+                                    _______,  _______,  _______,            _______,  _______,  _______
   ),
   // Nav
-  [3] = LAYOUT_split_3x6_3(
-      XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-	  KC_AGIN,         KC_PSTE,         KC_COPY,         KC_CUT,          KC_UNDO,         XXX,
-	  
-      XXX,             KC_LGUI,         KC_LALT,         KC_LCTL,         KC_LSFT,         XXX,
-	  CW_TOGG,         KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         XXX,
- 
-      XXX,             XXX,             KC_ALGR,         XXX,             XXX,             XXX,
-	  KC_INS,          KC_HOME,         KC_PGDN,         KC_PGUP,         KC_END,          XXX,
-      
-	  XXX,             XXX,             XXX,             KC_ENT,          KC_BSPC,         KC_BSLS
+  [NAV] = LAYOUT_split_3x6_3(
+      RM_TOGG,  XXX,      XXX,      XXX,      XXX,      XXX,                KC_AGIN,  KC_PSTE,  KC_COPY,  KC_CUT,   KC_UNDO,  XXX,
+      XXX,      KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  XXX,                CW_TOGG,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXX,
+      UG_NEXT,  XXX,      KC_ALGR,  XXX,      XXX,      XXX,                KC_INS,   KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXX,
+	                                _______,  _______,  _______,            _______,  _______,  _______
   ),
   // Fun
-    [4] = LAYOUT_split_3x6_3(
-      XXX,             KC_F12,          KC_F7,           KC_F8,           KC_F9,           KC_PSCR,
-	  XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-
-      XXX,             KC_F11,          KC_F4,           KC_F5,           KC_F6,           KC_SCRL,
-      XXX,             KC_LSFT,         KC_LCTL,         KC_LALT,         KC_LGUI,         XXX,
-		 
-      XXX,             KC_F10,          KC_F1,           KC_F2,           KC_F3,           KC_PAUS,
-      XXX,             XXX,             XXX,             KC_ALGR,         XXX,             XXX,
-		  
-      KC_APP,          KC_SPC,          KC_TAB,          XXX,             XXX,             XXX
+    [FUN] = LAYOUT_split_3x6_3(
+      RM_TOGG,  KC_F12,   KC_F7,    KC_F8,    KC_F9,    KC_PSCR,            XXX,      XXX,      XXX,      XXX,      XXX,      XXX,
+      XXX,      KC_F11,   KC_F4,    KC_F5,    KC_F6,    KC_SCRL,            XXX,      KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,  XXX,
+      UG_NEXT,  KC_F10,   KC_F1,    KC_F2,    KC_F3,    KC_PAUS,            XXX,      XXX,      XXX,      KC_ALGR,  XXX,      XXX,
+                                    KC_APP,   _______,  _______,            XXX,      XXX,      XXX
   ),
   // Num alternative
-  [5] = LAYOUT_split_3x6_3(
-      XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-	  XXX,             XXX,             XXX,             XXX,             XXX,             XXX,
-	  
-      XXX,             LGUI_T(KC_4),    LALT_T(KC_3),    LCTL_T(KC_2),    LSFT_T(KC_1),    XXX,
-	  XXX,             RSFT_T(KC_6),    RCTL_T(KC_7),    LALT_T(KC_8),    RGUI_T(KC_9),    XXX,
-
-      XXX,             XXX,             KC_RALT,         XXX,             KC_5,            XXX,
-	  XXX,             KC_0,            XXX,             KC_RALT,         XXX,             XXX,
-	    
-      KC_ESC,          KC_SPC,    		KC_TAB,          KC_ENT,          KC_BSPC,         KC_BSLS
-  ),
-  // Sym alternative
-  [6] = LAYOUT_split_3x6_3(
-      XXX,             XXX,             KC_LT,           KC_MINS,         KC_GT,           XXX,
-	  XXX,             KC_LBRC,         KC_UNDS,         KC_RBRC,         XXX,             XXX,
-	  
-      KC_EXLM,         KC_BSLS,         KC_LPRN,         KC_PLUS,         KC_RPRN,         KC_HASH,
-	  KC_PERC,         KC_LCBR,         KC_EQL,          KC_RCBR,         KC_DQT,          KC_QUES,
-	  
-      XXX,             XXX,             KC_SLSH,         KC_ASTR,         KC_HASH,         KC_PIPE,
-	  XXX,             KC_AMPR,         U_CIRC,          U_TILD,          XXX,             XXX,
-	  
-      KC_ESC,          KC_SPC,    		KC_TAB,          KC_ENT,          KC_BSPC,         KC_BSLS
+  [ALTNUM] = LAYOUT_split_3x6_3(
+      XXX,      XXX,      XXX,      XXX,      XXX,      XXX,                XXX,      XXX,      XXX,      XXX,      XXX,      XXX,
+      XXX,      HRM_4,    HRM_3,    HRM_2,    HRM_1,    XXX,                XXX,      HRM_6,    HRM_7,    HRM_8,    HRM_9,    XXX,
+      XXX,      XXX,      KC_RALT,  XXX,      KC_5,     XXX,                XXX,      KC_0,     XXX,      KC_RALT,  XXX,      XXX,
+	                                _______,  _______,  _______,            _______,  _______,  _______
   )
+  
+// [L_NAVIGATION] = LAYOUT(
+    // OS_UNDO,OS_CUT, OS_PAST,OS_COPY,OS_SALL,                KC_PGUP,KC_HOME,KC_UP,  KC_END, KC_DEL,
+    // KC_LGUI,KC_LALT,KC_LSFT,KC_LCTL,_______,                KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_BSPC,
+    // WIN_V,  _______,_______,OS_FIND,SEARCH, _______,_______,_______,SFT_TAB,KC_TAB, KC_APP, KC_PSCR,
+    // _______,_______,_______,NUMWORD,ooooooo,_______,_______,ooooooo,_______,_______,_______,_______
+// ),
+
 };
 
+
+// Combo definition
+const uint16_t PROGMEM c_ESC[] = {KC_A, KC_LPRN, COMBO_END};
+const uint16_t PROGMEM c_test[] = {KC_X, KC_K, COMBO_END};
+const uint16_t PROGMEM c_TOGG[] = {KC_A, KC_N, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(c_ESC,  KC_ESC),  // Esc
+	COMBO(c_test, KC_ESC),  // Esc
+    COMBO(c_TOGG, CW_TOGG)  // CAPSWORD
+};
+
+// enum combo_events {
+  // c_PRN,
+  // c_BRC,
+// };
+
+// const uint16_t PROGMEM PRN_combo[] = {KC_T, KC_N, COMBO_END};
+// const uint16_t PROGMEM BRC_combo[] = {KC_U, KC_V, COMBO_END};
+
+// combo_t key_combos[] = {
+  // [c_PRN] = COMBO_ACTION(PRN_combo),
+  // [c_BRC] = COMBO_ACTION(BRC_combo),
+// };
+
+
+// void process_combo_event(uint16_t combo_index, bool pressed) {
+  // switch(combo_index) {
+    // case c_PRN:
+      // if (pressed) {
+        // tap_code16(KC_LPRN);
+        // tap_code16(KC_LEFT);
+        // tap_code16(KC_RPRN);
+      // }
+      // break;
+    // case c_BRC:
+      // if (pressed) {
+        // tap_code16(KC_LBRC);
+        // tap_code16(KC_LEFT);
+        // tap_code16(KC_LBRC);
+      // }
+      // break;
+  // }
+// }
 
 
 // macro's
@@ -207,3 +232,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
